47eafe771f8116bb6e95ed907d5291f7cad435af
greg <tahir.gregory.dev@gmail.com>
2024-07-05 20:03:55 +0200

    mise en place du hearder et de la navbar

---
+++ script.js
@@ -0,0 +1,14 @@
+const menuBurger = document.querySelector(".button-burger");
+const navBar = document.querySelector("nav");
+
+const menuBurgerImage = document.querySelector(".burger-image");
+
+menuBurger.addEventListener("click", () => {
+	navBar.classList.toggle("open");
+  if (navBar.classList.contains("open")) {
+    menuBurgerImage.src = "/Images/x.svg";
+  } else {
+    menuBurgerImage.src = "/Images/menu_burger.svg"
+  }
+});
+
